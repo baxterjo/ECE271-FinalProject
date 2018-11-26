77bff2ee815eb2f43a8a033454ab5d0a4a82ee9f
Jordan Baxter <baxterjo@oregonstate.edu>
2018-11-26 12:28:56 -0800

    continued work on IR SPI, IR is hard.

--- FinalProjectTopLevel.sv
+++ FinalProjectTopLevel.sv
@@ -1,18 +1,19 @@
 module FinalProjectTopLevel(
-	input logic clk, reset,
+	input logic clk, reset, ps2In,
 	input logic [1:0] sel,
-	input logic [2:0] a,b,c,
+	input logic [2:0] a,b,
 	output logic vsync, hsync,
 	output logic[3:0] VGA_R, VGA_G, VGA_B
 	);
 
 // internal wires
 	logic clk25M, clk500, enable;
-	logic [2:0] command;
+	logic [2:0] command, ps2ToMux;
 	int Cx, Cy, Ox, Oy, Ow, Oh;
 
 // modules 
-	multiplexer mux0(a, b, c, sel, command);
+	PS2_SPID p0(clk, ps2In, ps2ToMux);
+	multiplexer mux0(a, b, ps2ToMux, sel, command);
 	ObjectLocation L0(~reset, clk500, command, Ox, Oy, Oh, Ow);
 	clkDivider #(100000) clk1(clk, ~reset, clk500);
 	clkDivider #(2) clk0(clk, ~reset, clk25M);
--- IRSPI.sv
+++ IRSPI.sv
@@ -1,13 +1,32 @@
 module IRSPI(
-	input logic IRsensor, clk,
+	input logic IRsensor, clk, reset,  // This SPI will use the RC5 code. 
 	output logic toDec
 	);
 	
+	int timer = 0;
 	int counter = 0;
+	logic read;
 
-	always_ff @(posedge IRsensor)
+	always_ff @(posedge Irsensor)
 	begin
-		counter <= 0;
-		always_ff @(posedge clk)
+		if (reset)
+		begin
+			counter = 0;
+			timer = 0;
+			read = 0;
+		end
+		else
 		begin
 			counter <= counter + 1;
+			if (counter < 890)
+			begin
+				read <= ~read;
+			end
+			always_ff @(posedge clk)  // Measure the time btween each pulse.
+			begin
+				timer <= timer + 1;
+				if(timer > 1300)
+				begin
+					counter <= 0;
+				end
+			end
